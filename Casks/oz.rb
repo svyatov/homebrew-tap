70f477faa13c8540ce905015469520a61c54affc
Leonid Svyatov <leonid@svyatov.com>
2026-04-03 00:13:44 +0300

    feat: add oz cask for v0.1.0

---
+++ Casks/oz.rb
@@ -0,0 +1,19 @@
+cask "oz" do
+  version "0.1.0"
+
+  on_intel do
+    sha256 "aeba0d67786268e8bdc83a671df271da6401277d4fb3c6bd8648d82c9e002d97"
+    url "https://github.com/svyatov/oz/releases/download/v#{version}/oz_#{version}_darwin_amd64.tar.gz"
+  end
+
+  on_arm do
+    sha256 "e496274e74c661a7f20d55fd3420794b01c58bf73a410d31be046b1fe631c5b4"
+    url "https://github.com/svyatov/oz/releases/download/v#{version}/oz_#{version}_darwin_arm64.tar.gz"
+  end
+
+  name "oz"
+  desc "Config-driven CLI wizard framework"
+  homepage "https://github.com/svyatov/oz"
+
+  binary "oz"
+end
